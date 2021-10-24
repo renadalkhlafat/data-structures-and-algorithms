066c2a2cd5bd171f0dc0212177df2804e9fdd4e5
renadalkhlafat <renadsalem8888@gmail.com>
2021-10-24 21:00:54 +0300

    test : add test for multiple enqueues

--- python/stack_and_queue/tests/test_stack_queue_pseudo.py
+++ python/stack_and_queue/tests/test_stack_queue_pseudo.py
@@ -1,8 +1,25 @@
 from stack_and_queue.stack_queue_pseudo import PseudoQueue
+import pytest
+
 
 def test_enqueue():
     expected = 5
-    queue = PseudoQueue()
-    queue.enqueue(5)
-    actual = queue.rear.data
+    pesudo_queue = PseudoQueue()
+    pesudo_queue.enqueue(5)
+    actual = pesudo_queue.rear.data
     assert expected == actual
+
+def test_multiple_enqueues(pesudo_queue):
+    expected = 3
+    actual = pesudo_queue.rear.data
+    assert expected == actual
+
+
+@pytest.fixture
+def queue():
+    pesudo_queue =PseudoQueue()
+    pesudo_queue.enqueue(3)
+    pesudo_queue.enqueue(6)
+    pesudo_queue.enqueue(8)
+
+    return pesudo_queue
