b418556220e59684ba6b4a7c7b1bf0130da901d2
renadalkhlafat <renadsalem8888@gmail.com>
2021-10-24 20:51:45 +0300

    prepare the PesudoQueue test file

--- python/stack_and_queue/tests/test_stack_queue_pseudo.py
+++ python/stack_and_queue/tests/test_stack_queue_pseudo.py
@@ -1,2 +1,8 @@
 from stack_and_queue.stack_queue_pseudo import PseudoQueue
 
+def test_enqueue():
+    expected = 5
+    queue = PseudoQueue()
+    queue.enqueue(5)
+    actual = queue.rear.data
+    assert expected == actual
