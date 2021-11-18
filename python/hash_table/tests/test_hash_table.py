cf58c3bfa50ac63a6a9a2d72c31cca761d5cdf40
renadalkhlafat <renadsalem8888@gmail.com>
2021-11-18 19:29:52 +0200

    test:Adding a key/value to your hashtable results in the value being in the data structure

--- python/hash_table/tests/test_hash_table.py
+++ python/hash_table/tests/test_hash_table.py
@@ -1,5 +1,31 @@
 from hash_table import __version__
+from hash_table.hash_table import HashTable
+import pytest
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+@pytest.fixture
+def hashtable():
+	return HashTable()
+
+
+def test_hash(hashtable):
+	expected = 700
+	actual = hashtable._HashTable__hash("d")
+	assert actual == expected
+
+
+def test_hash_word(hashtable):
+	expected = 376
+	actual = hashtable._HashTable__hash("dd")
+	assert actual == expected
+
+
+def test_add_to_hash_table(hashtable):
+    expected = "23"
+    hashtable.add("renad","23")
+    actual = hashtable.get("renad")
+    assert actual == expected
