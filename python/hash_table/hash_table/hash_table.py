df228f5dc0b0286d221a35dc83547ff3db254a68
renadalkhlafat <renadsalem8888@gmail.com>
2021-11-25 19:28:51 +0200

    fix left join function

--- python/hash_table/hash_table/hash_table.py
+++ python/hash_table/hash_table/hash_table.py
@@ -34,7 +34,15 @@ class HashTable:
         """
         self.__size = size
         self.__buckets = [None] * size
-
+    def __iter__(self):
+        left_join = []
+        for item in self.__buckets:
+            if item :
+                current = item.head
+                left_join.append(current.value[0])
+                left_join.append(current.value[1])
+                yield left_join
+                
     def __hash(self, key):
         """
         Takes a key which is a string and returns an integer which is the index that will be used to store the key/value pari in a Node at that index.
