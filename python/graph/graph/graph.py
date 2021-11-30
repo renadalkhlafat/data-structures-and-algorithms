a8047f5baecbaa8e204fa839b4e570e3163d4c59
renadalkhlafat <renadsalem8888@gmail.com>
2021-11-30 21:01:20 +0200

    fixed business trip

--- python/graph/graph/graph.py
+++ python/graph/graph/graph.py
@@ -136,19 +136,24 @@ class Graph:
         return final_result
 
 def business_trip(graph ,city_list):
-    city_names = [city.value for city in city_list]
     cost = 0
-    cities = graph.get_neighbors(city_list[0])
-
-    for city in cities:
-        print(city.vertex.value)
-        if city.vertex.value in city_names:
-            cost += city.weight
-
-    result = [True , f"${cost}"]
+    def walk(city_list):
+        nonlocal cost
+        first_city = city_list.pop(0)
+        cities = graph.get_neighbors(first_city)
+        v = [city.vertex for city in cities]
+        for city in cities:
+            if city_list[0] not in v:
+                return [False , "$0"]
+            if city.vertex in city_list:
+                print(city.weight)
+                cost += city.weight
+                if len(city_list)>1 :
+                    walk(city_list)
+    walk(city_list)
     if not cost:
         return [False , "$0"]
-    return result
+    return [True , f"${cost}"]
 
 
 
